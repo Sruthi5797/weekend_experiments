71a861546a57f6edb58a9c7a2d91dde9988c7f60
Mr-Saab-29 <sabariravi29597@gmail.com>
2024-06-12 15:52:54 +0200

    Using Groq to generate response instead of Ollama

--- pickup_cupid/src/app.py
+++ pickup_cupid/src/app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import os
+from dotenv import find_dotenv, load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_community.llms import Ollama
+from langchain_groq import ChatGroq
 
 def model_inference(radio_options):
     llm_call = Ollama(model="llama3") 
@@ -12,6 +15,24 @@ def model_inference(radio_options):
     )
     pickupline_output = llm_call.invoke(prompt.format(selected_option=radio_options))
     return pickupline_output
+
+def model_inference2(radio_options):
+    env_path = find_dotenv()
+
+    load_dotenv(env_path)
+
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+    llm = ChatGroq(temperature=0, model_name="llama3-8b-8192")
+
+    response = PromptTemplate.from_template(
+        """Generate me a pick-up line according to the choosen scenario {selected_option}.
+        Output must only be the response, Keep it to a maximum of two lines only.
+        Don't add anything extra.
+        """
+    )
+    output = llm.invoke(response.format(selected_option = radio_options))
+    return output.content
 
 
 original_title = '<p style="font-family:Fantasy; color:Blue; font-size: 20px;">Pickup Cupid - Generate awesome pickup lines for your loved ones</p>'
@@ -25,7 +46,7 @@ radio_options = st.radio(
 st.write("You have selected",radio_options)
 
 if st.button("Get line"):
-    output = model_inference(radio_options)
+    output = model_inference2(radio_options)
     answer = st.text_area('Here is one for you',output)
     
 
